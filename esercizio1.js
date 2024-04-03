b1c4ab7ebabd79e2f2aad2b2cfaf169f0f6f1a03
= <=>
2024-04-03 13:44:58 +0200

    eros areyou memorize

---
+++ esercizio1.js
@@ -0,0 +1,10 @@
+let ciao = " hello word bhooo";
+
+console.log(ciao);
+
+//ricorda il global name e =
+// ricorda il local name e "eros greco"
+// ricorda il global email e =
+// ricorda il local emil e "erosgreco29@gmail.com"
+
+eros55;
